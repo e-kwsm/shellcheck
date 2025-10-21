936eaa9bb236c7056614385d3b295ca42690b6c5
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-10-22 02:50:01 +0900

    shellcheck.hs

--- shellcheck.hs
+++ shellcheck.hs
@@ -263,7 +263,7 @@ parseEnum name value list =
         Just value -> return value
         Nothing -> do
             printErr $ "Unknown value for --" ++ name ++ ". " ++
-                       "Valid options are: " ++ (intercalate ", " $ map fst list)
+                       "Valid options are: " ++ intercalate ", " (map fst list)
             throwError SupportFailure
 
 parseColorOption value =
@@ -341,7 +341,7 @@ parseOption flag options =
         Flag "source-path" str -> do
             let paths = splitSearchPath str
             return options {
-                sourcePaths = (sourcePaths options) ++ paths
+                sourcePaths = sourcePaths options ++ paths
             }
 
         Flag "sourced" _ ->
@@ -382,7 +382,7 @@ parseOption flag options =
         Flag "enable" value ->
             let cs = checkSpec options in return options {
                 checkSpec = cs {
-                    csOptionalChecks = (csOptionalChecks cs) ++ split ',' value
+                    csOptionalChecks = csOptionalChecks cs ++ split ',' value
                 }
             }
 
@@ -555,14 +555,14 @@ ioInterface options files = do
             find original original
       where
         find filename deflt = do
-            sources <- findM ((allowable rcSuggestsExternal inputs) `andM` doesFileExist) $
-                        (adjustPath filename):(map ((</> filename) . adjustPath) $ sourcePathFlag ++ sourcePathAnnotation)
+            sources <- findM (allowable rcSuggestsExternal inputs `andM` doesFileExist) $
+                        adjustPath filename:map ((</> filename) . adjustPath) (sourcePathFlag ++ sourcePathAnnotation)
             case sources of
                 Nothing -> return deflt
                 Just first -> return first
         scriptdir = dropFileName currentScript
         adjustPath str =
-            case (splitDirectories str) of
+            case splitDirectories str of
                 ("SCRIPTDIR":rest) -> joinPath (scriptdir:rest)
                 _ -> str
 
