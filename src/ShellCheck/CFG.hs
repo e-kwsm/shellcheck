f8eec4bf6ac035ecf63f52637adc67a071c7ac08
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-06-10 02:23:32 +0900

    fixup! refactor: move reverse out

--- src/ShellCheck/CFG.hs
+++ src/ShellCheck/CFG.hs
@@ -1131,7 +1131,7 @@ handleCommand cmd vars args literalCmd = do
             return (getId arg, name)
 
         getFromFallback =
-            listToMaybe $ reverse (mapMaybe getIfVar args)
+            listToMaybe $ reverse $ mapMaybe getIfVar args
         getIfVar c = do
             name <- getLiteralString c
             guard $ isVariableName name
